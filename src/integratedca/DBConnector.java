fd2a06b331aef6a690b7c9f9f2332f529cfe7e23
sba23150 <sba23150@student.cct.ie>
2024-03-17 19:41:16 +0000

    Database connection

---
+++ src/integratedca/DBConnector.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package integratedca;
+
+/**
+ *
+ * @author User
+ */
+public class DBConnector {
+    private final String DB_URL = "jdbc:mysql://localhost";
+    private final String USER = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+}
