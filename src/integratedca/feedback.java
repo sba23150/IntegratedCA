31cf277efb517aeb009700dcd5bab3d3e005b622
sba23150 <sba23150@student.cct.ie>
2024-03-23 15:56:07 +0000

    class feedback as per feedback table

--- src/integratedca/feedback.java
+++ src/integratedca/feedback.java
@@ -9,5 +9,38 @@ package integratedca;
  * @author User
  */
 public class feedback {
+    int id;
+    String Name;
+    String Course;
+    String Enrolment;
+    double grades; 
+    String feedback;
     
+    public feedback (int id, String Name, String Course, String Enrolment, double grades, String feedback){
+        this.id = id;
+        this.Name = Name;
+        this.Course = Course;
+        this.Enrolment = Enrolment;
+        this.grades = grades; 
+        this.feedback = feedback;
+    }
+    
+    public int getid(){
+        return id;
+    }
+    public String getName(){
+        return Name;
+    }
+    public String getCourse(){
+        return Course;
+    }
+    public String getEnrolment(){
+        return Enrolment;
+    }
+    public double getgrades(){
+        return grades;
+    }
+    public String getfeedback(){
+        return feedback;
+    }
 }
